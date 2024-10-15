4b85e92c6e75858c0ca7f4999e95462e0eefec9a
Ishwar749 <19itish083@ldce.ac.in>
2024-10-15 23:25:03 +0530

    The Values You Can Make

--- src/dynamicProgramming/introToDP/knapsackAndBasicDynamicProgramming/TheValuesYouCanMake.java
+++ src/dynamicProgramming/introToDP/knapsackAndBasicDynamicProgramming/TheValuesYouCanMake.java
@@ -6,20 +6,55 @@ import java.util.*;
 public class TheValuesYouCanMake {
 
     static int dp[][];
+
     public static void main(String args[]) {
 
         FastIO in = new FastIO();
 
-        int n = in.nextInt();
-        int k = in.nextInt();
+        int N = in.nextInt();
+        int K = in.nextInt();
 
-        int coins[] = new int[n];
+        int coins[] = new int[N];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             coins[i] = in.nextInt();
         }
 
 
+        boolean dp[][][] = new boolean[N][K + 1][K + 1];
+        dp[0][0][0] = true;
+        if (coins[0] <= K) {
+            dp[0][coins[0]][0] = true;
+            dp[0][coins[0]][coins[0]] = true;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j <= K; j++) {
+                for (int k = 0; k <= K; k++) {
+
+                    boolean usedInJandK = false;
+                    if (j - coins[i] >= 0 && k - coins[i] >= 0) usedInJandK = dp[i - 1][j - coins[i]][k - coins[i]];
+
+                    boolean usedInJ = false;
+                    if (j - coins[i] >= 0) usedInJ = dp[i - 1][j - coins[i]][k];
+
+                    boolean notUsedInBoth = dp[i - 1][j][k];
+
+                    dp[i][j][k] = usedInJandK || usedInJ || notUsedInBoth;
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int k = 0; k <= K; k++) {
+            if (dp[N - 1][K][k]) result.add(k);
+        }
+
+        in.println(result.size());
+        for (int e : result) in.print(e + " ");
+
+        in.close();
     }
 
 
---
+++ src/dynamicProgramming/introToDP/knapsackAndBasicDynamicProgramming/WhereIsTheGhost.java
@@ -0,0 +1,115 @@
+package dynamicProgramming.introToDP.knapsackAndBasicDynamicProgramming;
+
+import java.io.*;
+import java.util.*;
+
+public class WhereIsTheGhost {
+
+    static long mod = (long)(1e9 + 7);
+    public static void main(String args[]){
+
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int a[] = new int[n];
+
+        for(int i =0; i<n; i++){
+            a[i] = in.nextInt();
+        }
+
+        long dp[][] = new long[n][(1<<8)+1];
+        for(long e[]: dp){
+            Arrays.fill(e, -1);
+        }
+
+        long ans = find(0,1<<8,a,dp);
+
+        out.println(ans);
+        out.close();
+    }
+
+    static long find(int cur, int bits, int a[], long dp[][]){
+
+        if(cur==a.length){
+            if((bits & ((1 << 9) - 1)) == ((1 << 9) - 1)) return 1;
+            return 0;
+        }
+
+        int store = bits;
+        if(dp[cur][store]!=-1) return  dp[cur][store];
+
+        long ans = 0;
+
+        ans = find(cur+1,bits,a,dp)%mod;
+
+        if(a[cur]%7==0) bits = (bits&1);
+        if(a[cur]%5==0) bits = (bits&(1<<1));
+
+        int temp = a[cur];
+
+        if(temp%3==0){
+            if((bits&(1<<2))==0) bits = (bits&(1<<2));
+            else if((bits&(1<<3))==0) bits=  (bits&(1<<3));
+            temp = temp/3;
+        }
+
+        if(temp%3==0){
+            if((bits&(1<<3))==0) bits=  (bits&(1<<3));
+        }
+
+        temp = a[cur];
+
+        if(temp%2==0){
+            if((bits&(1<<4))==0) bits = (bits&(1<<2));
+            else if((bits&(1<<5))==0) bits=  (bits&(1<<3));
+            else if((bits&(1<<6))==0) bits = (bits&(1<<2));
+            temp = temp/2;
+        }
+
+        if(temp%2==0){
+            if((bits&(1<<5))==0) bits=  (bits&(1<<3));
+            else if((bits&(1<<6))==0) bits = (bits&(1<<2));
+            temp = temp/2;
+        }
+
+        if(temp%2==0){
+            if((bits&(1<<6))==0) bits = (bits&(1<<2));
+        }
+
+        ans = (ans%mod + find(cur+1,bits,a,dp)%mod)%mod;
+
+        dp[cur][store] = ans;
+        return ans;
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
