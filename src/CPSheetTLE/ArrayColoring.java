5ce9461fc21af62f4c6e8e1d2ed47c96ff4c3689
Ishwar749 <19itish083@ldce.ac.in>
2024-12-20 20:35:58 +0530

    CP Array Coloring

---
+++ src/CPSheetTLE/ArrayColoring.java
@@ -0,0 +1,58 @@
+package CPSheetTLE;
+
+import java.io.*;
+import java.util.*;
+
+// Problem: https://codeforces.com/problemset/problem/1857/A
+
+public class ArrayColoring {
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int tests = in.nextInt();
+
+        while (tests-- > 0) {
+            int n = in.nextInt();
+            int odds = 0;
+
+            for (int i = 0; i < n; i++) {
+                int x = in.nextInt();
+                if (x % 2 == 1) odds++;
+            }
+
+            if (odds % 2 == 0) out.println("YES");
+            else out.println("NO");
+        }
+        out.close();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
