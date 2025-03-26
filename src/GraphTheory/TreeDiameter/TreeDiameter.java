a526a2b8854d81b6f53b564808b13f7fa29ded4d
Ishwar749 <19itish083@ldce.ac.in>
2025-03-27 00:48:25 +0530

    Graph Tree Diameter TreeDiameter

---
+++ src/GraphTheory/TreeDiameter/TreeDiameter.java
@@ -0,0 +1,201 @@
+package GraphTheory.TreeDiameter;
+
+import java.io.*;
+import java.util.*;
+
+// Problem: https://cses.fi/problemset/task/1131
+
+public class TreeDiameter {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Map<Integer, List<Integer>> Tree = new HashMap<>();
+
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) Tree.put(i, new ArrayList<>());
+
+        for (int i = 0; i < n - 1; i++) {
+            int u = in.nextInt() - 1;
+            int v = in.nextInt() - 1;
+            Tree.get(u).add(v);
+            Tree.get(v).add(u);
+        }
+
+        int[] answer = new int[1];
+        maxDistance(0, -1, Tree, answer);
+
+        out.println(answer[0]);
+        out.close();
+    }
+
+    static Integer maxDistance(int cur, int par, Map<Integer, List<Integer>> Tree, int[] answer) {
+
+        int max1 = 0;
+        int max2 = 0;
+
+        for (int child : Tree.get(cur)) {
+            if (child != par) {
+                int dist = maxDistance(child, cur, Tree, answer);
+                if (dist > max1) {
+                    max2 = max1;
+                    max1 = dist;
+                } else max2 = Math.max(max2, dist);
+            }
+        }
+
+        answer[0] = Math.max(answer[0], max1 + max2);
+        return max1 + 1;
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+}
