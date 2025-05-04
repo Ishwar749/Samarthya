b66cd4529074663e34cbc46b251716a5e94388c4
Ishwar749 <19itish083@ldce.ac.in>
2025-05-05 00:17:18 +0530

    Graph TopoSorting Find Permutation

---
+++ src/GraphTheory/TopologicalSorting/FindPermutation.java
@@ -0,0 +1,215 @@
+package GraphTheory.TopologicalSorting;
+
+import java.io.*;
+import java.util.*;
+
+// Problem: https://atcoder.jp/contests/abc291/tasks/abc291_e
+
+public class FindPermutation {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) graph.put(i, new ArrayList<>());
+        int[] inDegree = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int v = in.nextInt() - 1;
+            int u = in.nextInt() - 1;
+            graph.get(u).add(v);
+            inDegree[v]++;
+        }
+
+        int[] answer = new int[n];
+        Arrays.fill(answer, -1);
+        int num = n;
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) if (inDegree[i] == 0) q.add(i);
+
+        boolean isUnique = true;
+
+        while (!q.isEmpty()) {
+            if (q.size() != 1) {
+                isUnique = false;
+                break;
+            }
+            int cur = q.poll();
+            answer[cur] = num;
+            num--;
+
+            for (int nei : graph.get(cur)) {
+                inDegree[nei]--;
+                if (inDegree[nei] == 0) q.add(nei);
+            }
+        }
+
+        if (!isUnique || !isPossible(answer)) out.println("No");
+        else {
+            out.println("Yes");
+            for (int e : answer) out.print(e + " ");
+        }
+        out.close();
+    }
+
+    static boolean isPossible(int[] answer) {
+        for (int e : answer) if (e == -1) return false;
+        return true;
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+}
